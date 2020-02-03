be1b04da2e3e0944a54c7a20de4f9b78898e5482
yuheng.wang <yuheng.wang@thoughtworks.com>
2020-02-03 16:32:43 +0800

    完成FizzBuzz功能

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -5,22 +5,18 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
     private static final String WHIZZ = "Whizz";
+    private static final String EMPTY_STRING = "";
 
     public String execute(int number) {
-        if (!numberContainChar('5', number)
-                && numberContainChar('3', number)) {
+        if (!numberIsContainChar('5', number)
+                && numberIsContainChar('3', number)) {
             return FIZZ;
         }
-        String count = "";
-        if (number % 3 == 0) {
-            count += FIZZ;
-        }
-        if (number % 5 == 0) {
-            count += BUZZ;
-        }
-        if (number % 7 == 0) {
-            count += WHIZZ;
-        }
+        StringBuilder temp = new StringBuilder();
+        temp.append(getFizzByNumber(number));
+        temp.append(getBuzzByNumber(number));
+        temp.append(getWhizzByNumber(number));
+        String count = temp.toString();
         return stringIsEmpty(count) ? String.valueOf(number) : count;
     }
 
@@ -28,8 +24,20 @@ public class FizzBuzz {
         return input == null || input.length() == 0;
     }
 
-    private boolean numberContainChar(char contained, int number) {
+    private boolean numberIsContainChar(char contained, int number) {
         String temp = String.valueOf(number);
         return temp.contains(String.valueOf(contained));
+    }
+
+    private String getFizzByNumber(int number) {
+        return number % 3 == 0 ? FIZZ : EMPTY_STRING;
+    }
+
+    private String getBuzzByNumber(int number) {
+        return number % 5 == 0 ? BUZZ : EMPTY_STRING;
+    }
+
+    private String getWhizzByNumber(int number) {
+        return number % 7 == 0 ? WHIZZ : EMPTY_STRING;
     }
 }
