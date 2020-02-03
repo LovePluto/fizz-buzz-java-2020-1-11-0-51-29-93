c3cf94bdd9a310196d817eb92d947b2d2b4585c0
yuheng.wang <yuheng.wang@thoughtworks.com>
2020-02-03 15:43:14 +0800

    添加了AC的失败测试用例

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -7,12 +7,42 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void when_number_return_number() {
+    public void when_number_is_1_return_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         Integer input = 1;
 
         String result = fizzBuzz.execute(input);
 
         assertEquals("1", result);
+    }
+
+    @Test
+    public void when_number_is_3_return_Fizz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Integer input = 3;
+
+        String result = fizzBuzz.execute(input);
+
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void when_number_is_5_return_Buzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Integer input = 5;
+
+        String result = fizzBuzz.execute(input);
+
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void when_number_is_7_return_Whizz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Integer input = 7;
+
+        String result = fizzBuzz.execute(input);
+
+        assertEquals("Whizz", result);
     }
 }
