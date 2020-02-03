1a97b76cc0596e0d365762f890d398ff7ca63137
yuheng.wang <yuheng.wang@thoughtworks.com>
2020-02-03 15:45:16 +0800

    实现了AC2的功能

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,7 +1,16 @@
 package com.thoughtworks;
 
 public class FizzBuzz {
-    public String execute(int i) {
-        return String.valueOf(i);
+    public String execute(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        if (number % 7 == 0) {
+            return "Whizz";
+        }
+        return String.valueOf(number);
     }
 }
